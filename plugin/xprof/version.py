5d58a4c0d3aba8e4b7dde436acd7561594da4760
Yunlong Liu <yunlongl@x.ai>
2025-09-19 05:08:10 +0000

    update version for local build

--- plugin/xprof/version.py
+++ plugin/xprof/version.py
@@ -14,4 +14,4 @@
 # ==============================================================================
 """Version information for tensorboard-plugin-profile."""
 
-__version__ = "2.20.5"
+__version__ = "2.20.5.2"
